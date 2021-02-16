516ce3ad7ae63610064abdec43aec49e81f325a8
Azuthedog1 <sb.and.bigsur@gmail.com>
2021-02-16 01:49:56 -0800

    added css and javascript and html

--- myscript.js
+++ myscript.js
@@ -1,3 +1,30 @@
 $(document).ready(function() {
 	
+
+	//page 3
+	$("#springtext").click(function(){
+		$("#springcal").toggle();
+		});
+	$("#summertext").click(function(){
+		$("#summercal").toggle();
+		});
+	$("#falltext").click(function(){
+		$("#fallcal").toggle();
+		});
+	$("#wintertext").click(function(){
+		$("#wintercal").toggle();
+		});
+
+	$("#spring").click(function(){
+		$(".spring").toggle();
+		});
+	$("#summer").click(function(){
+		$(".summer").toggle();
+		});
+	$("#fall").click(function(){
+		$(".fall").toggle();
+		});
+	$("#winter").click(function(){
+		$(".winter").toggle();
+		});
 });
